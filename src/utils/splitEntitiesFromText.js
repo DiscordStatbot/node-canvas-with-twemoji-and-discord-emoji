0d894e37e310477c6cc96f80e54c1847068ba3f2
lando <calrissian@opayq.com>
2023-01-11 06:23:27 +0000

    hotfix

--- src/utils/splitEntitiesFromText.js
+++ src/utils/splitEntitiesFromText.js
@@ -26,7 +26,7 @@ function parseDiscordEmojis(textEntities) {
 }
 
 module.exports = function splitEntitiesFromText(text) {
-  const twemojiEntities = parse(text, { assetType: "png" });
+  const twemojiEntities = parse(text, { assetType: "png", base: 'https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/' });
 
   let unparsedText = text;
   let lastTwemojiIndice = 0;
